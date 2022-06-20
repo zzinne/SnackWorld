f2009f873e4dfb3d7d03cd21d1de619b3057981d
zzinne <zzinee1005@naver.com>
2022-06-20 23:06:33 +0900

    사용자 클래스 추가

---
+++ src/main/java/snack/User.java
@@ -0,0 +1,13 @@
+package snack;
+
+public class User {
+    String name;
+    String authority;
+    int totalAmount;
+
+    User(String name,String authority,int totalAmount){
+        this.name = name;
+        this.authority = authority;
+        this.totalAmount = totalAmount;
+    }
+}
