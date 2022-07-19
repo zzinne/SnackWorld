3c96667ff2cfe3a92f5be89f4057c23fca9e1b0b
zzinne <zzinee1005@naver.com>
2022-07-19 22:52:41 +0900

    리팩토리 진행중 서버 에러 수정중

--- src/main/java/util/HttpRequestUtils.java
+++ src/main/java/util/HttpRequestUtils.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 
 import java.util.Arrays;
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class HttpRequestUtils {
@@ -29,8 +30,8 @@ public class HttpRequestUtils {
         String[] tokens = values.split(separator);
         return Arrays.stream(tokens)
                 .map( t->getKeyValue(t,"="))
-                .filter(p ->p != null)
-                .collect(Collectors.toMap(p->p.getKey(),p->p.getValue()));
+                .filter(Objects::nonNull)
+                .collect(Collectors.toMap(Pair::getKey,Pair::getValue));
     }
 
     static Pair getKeyValue(String keyValue, String regex){
@@ -53,7 +54,12 @@ public class HttpRequestUtils {
             this.key = key;
             this.value = value;
         }
-
+        public String getKey(){
+            return key;
+        }
+        public String getValue(){
+            return value;
+        }
         @Override
         public String toString(){
             return "Pair [key="+key+"value="+value+"]";
